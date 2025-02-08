bd072c364d9f05fbb28fa3b82fa85581ea2e3c20
sthodeme <tsreeram@gmail.com>
2025-02-08 11:53:04 +0100

    commit

--- MyJLists/src/Main.java
+++ MyJLists/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        MyWindow mw = new MyWindow();
+
+            MyWindow mw = new MyWindow();
+            mw.setVisible(true);
     }
 }
--- MyJLists/src/MyWindow.java
+++ MyJLists/src/MyWindow.java
@@ -1,2 +1,31 @@
-public class MyWindow {
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MyWindow extends JFrame{
+    private static final long serialVersionUID = 1487225919900733810L;
+    //constructor
+    public void MyWindow(){
+        //set title
+        this.setTitle("My List Collection");
+        this.setSize(200, 400);
+        this.setLayout(null);
+        this.setResizable(false);
+
+        //closing window
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    /*
+        JLabel jLabel01 = new JLabel("jLable01: test text");
+        jLabel01.setSize(150,20);
+        this.add(jLabel01);
+    */
+        JTextField input01 = new JTextField();
+        input01.setBounds(5, 5, 100, 20);
+        //input01.setEditable(false);
+        this.add(input01);
+
+
+        //make everything visible
+        this.setVisible(true);
+    }
 }
