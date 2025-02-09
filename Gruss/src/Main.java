0e6864f2fc594aa7b4e6ca62cabe23d0aa2b5ab5
sthodeme <tsreeram@gmail.com>
2025-02-09 13:14:56 +0100

    commit

--- Gruss/src/Main.java
+++ Gruss/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         MainWindow mw =new MainWindow();
-        System.out.println();
+        //System.out.println();
 
     }
 }
--- Gruss/src/MainWindow.java
+++ Gruss/src/MainWindow.java
@@ -1,7 +1,62 @@
-public class MainWindow {
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+//import javax.swing.*;
+public class MainWindow extends JFrame{
+
+    private static final long serialVersionUID = 1L;
 
     //constructor
-    public void MainWindow(){
+    public void MainWindow() {
+        
+        //JFrame
+        this.setTitle("Grusstabelle");
+        this.setSize(20, 300);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
 
+        //JLabel
+        JLabel label01 = new JLabel("Name:");
+        label01.setEnabled(false);
+        label01.setBounds(10, 10, 200, 15);
+        this.add(label01);
+
+        //JTesxtField
+
+        JTextField name = new JTextField();
+        name.setBounds(10, 35, 200, 15);
+        name.setEditable(true);
+        this.add(name);
+
+        //JButton
+        JButton nameButton = new JButton("Gruss");
+        nameButton.setBounds(10, 60, 25, 15);
+        this.add(nameButton);
+
+        //ActionListener
+        //String copyName;
+        ActionListener takeName = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == nameButton) {
+                //String copyName = name.getText();
+                System.out.println("Hallo " + name);
+                JOptionPane grussPane = new JOptionPane();
+                grussPane.showMessageDialog(null, "Hallo " + name, "Gruss", JOptionPane.INFORMATION_MESSAGE);
+
+                }
+            }
+        };
+
+        
+
+        this.setVisible(true);
     }
 }
