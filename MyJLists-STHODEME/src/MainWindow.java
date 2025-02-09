009d933eca2764d414e61b6a30f39320b0173d0c
sthodeme <tsreeram@gmail.com>
2025-02-09 09:34:44 +0100

    commit

--- MyJLists-STHODEME/src/MainWindow.java
+++ MyJLists-STHODEME/src/MainWindow.java
@@ -4,6 +4,14 @@ import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.plaf.synth.ColorType;
+
+import java.lang.String.*;
+import java.util.stream.Collector;
+import java.awt.Color;
+import java.awt.Font;
 
 //import com.ibm.j9ddr.vm29.pointer.generated.statPointer;
 
@@ -29,24 +37,77 @@ public class MainWindow extends JFrame{
         this.add(textField01);
         
         //JLists
+        //labelText and its selection listener
         String[] labelText = {"One", "Two", "Three", "Four", "Five"};
-        JList<String> textlist = new JList<>(labelText);
-        textlist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        textlist.setBounds(10, 70 , 80, 100);
-        this.add(textlist);
+        JList<String> textList = new JList<>(labelText);
+        textList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        textList.setBounds(10, 70 , 80, 100);
+        this.add(textList);
 
+        textList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()){
+                    jlabel1.setText(textList.getSelectedValue());
+                }
+                
+            }
+        });
+        
+
+        //labelColor and its selection listener
         String[] textColor = {"Red", "Green", "Blue"};
         JList<String> colorList = new JList<>(textColor);
         colorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         colorList.setBounds(10, 200 , 80, 60);
         this.add(colorList);
 
+        colorList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()){
+                    String selectedColor = colorList.getSelectedValue();
+                    
+                    Color color;
+                    switch (selectedColor) {
+                        case "Red":
+                            color = Color.RED;
+                            break;
+                        case "Green":
+                            color = Color.GREEN;
+                            break;
+                        case "Blue":
+                            color = Color.BLUE;
+                            break;
+                        default:
+                            color = Color.BLACK;
+                            break;
+                    }
+                    jlabel1.setForeground(color);
+                }
+            }
+        });
+
+        //labelSize and its selection listener
         Integer[] textSize = {8, 10, 12, 14, 16, 18, 20};
         JList<Integer> textSizeList = new JList<>(textSize);
         JScrollPane textSizeListPane = new JScrollPane(textSizeList);
         textSizeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         textSizeListPane.setBounds(10, 300 , 80, 100);
         this.add(textSizeListPane);
+
+        //labelSizeList selection listener
+        textSizeList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()){
+                    float fontSize = textSizeList.getSelectedValue();
+                    Font jLebelTextFont = jlabel1.getFont();
+                    Font jLebelTextNewFont = jLebelTextFont.deriveFont(fontSize);
+                    jlabel1.setFont(jLebelTextNewFont);
+                }
+            }
+        });
 
         
 
