4a6d3fe46f1113ce62d50c3f9d783dea02a39835
sthodeme <tsreeram@gmail.com>
2025-02-14 06:18:13 +0100

    commit

--- Calculator-STHODEME/src/CalculatorWindow.java
+++ Calculator-STHODEME/src/CalculatorWindow.java
@@ -1,25 +1,34 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+@SuppressWarnings("serial")
 
 public class CalculatorWindow extends JFrame {
     private static final long serialVersionUID = 1L;
 
+    JTextField inputTextField; //to access the content, even after the window run is over
+
 	public CalculatorWindow() {
         this.setTitle("Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(230, 400);
+        setSize(250, 400);
+        this.getContentPane().setBackground(Color.LIGHT_GRAY);
         setLocationRelativeTo(null);
         setLayout(null);
 
-        /* 
+        // set Resource 
+        /*/
+        CalcEngine calcEngine = new CalcEngine(this);
+        Font displayFont = new Font("Ink Free", Font.BOLD, 30);
+        Font digitsFont = new Font("Ink Free", Font.BOLD, 30);
+        this.display = new JLabel();
+        */
+        
         //Jlabel to show first number while the second number is being entered
         JLabel firstNumberLabel = new JLabel("xxx");
         firstNumberLabel.setBounds(70, 5, 100, 20);
@@ -30,10 +39,11 @@ public class CalculatorWindow extends JFrame {
         JLabel operatorLabel = new JLabel("+");
         operatorLabel.setBounds(180, 5, 30, 20);
         this.add(operatorLabel);
-        */
 
         //Jlabel to show the (first & ) second number being entered
         JLabel label01 = new JLabel("Input:");
+        int xPos = 5, yPos = 5, xWidth = 50, yHeight = 50;
+
         label01.setBounds(5, 25, 50, 20);
         this.add(label01);
 
@@ -41,9 +51,14 @@ public class CalculatorWindow extends JFrame {
         JTextField inputTextField = new JTextField();
         inputTextField.setBounds(60, 25, 150, 20);
         inputTextField.setBackground(Color.YELLOW);
+        inputTextField.setHorizontalAlignment(JTextField.RIGHT);
         inputTextField.setEditable(false); 
+        inputTextField.setOpaque(true);
         this.add(inputTextField);
-
+        /*/
+        ActionListener dl = calcEngine.getDigitListener();
+        ActionListener ol = calcEngine.getOperatorListener();
+        */
         //Jlabel
         JLabel label02 = new JLabel("Output:");
         label02.setBounds(5, 45, 50, 20);
@@ -56,67 +71,74 @@ public class CalculatorWindow extends JFrame {
         outputTextField.setBackground(Color.GREEN);
         this.add(outputTextField);
      
-        //JButton  array for numbers 0-9
-        JButton[] numberButtons = new JButton[10];
+        //JButton "0-9", '.' and '+/-'
+        JButton[] numberButtons = new JButton[12];
+
+        //JButton  array initialization for numbers 0-9
         for (int i = 0; i < 10; i++){
             numberButtons[i] = new JButton(String.valueOf(i));
         }
-        //JButton "0-9" positioning in JFrame
+        //JButton  array initialization for '.' and '+/-' buttons
+        numberButtons[10] = new JButton(".");
+        numberButtons[11] = new JButton("+/-");
+        //JButton "0-9", "." & "+/-" positioning in JFrame
         // (this is required, when no layout manager is used)
-        numberButtons[0].setBounds(5, 80, 50, 50);
-        numberButtons[1].setBounds(60, 80, 50, 50);
-        numberButtons[2].setBounds(115, 80, 50, 50);
-        numberButtons[3].setBounds(170, 80, 50, 50);
-        numberButtons[4].setBounds(5, 135, 50, 50);
-        numberButtons[5].setBounds(60, 135, 50, 50);
-        numberButtons[6].setBounds(115, 135, 50, 50);
-        numberButtons[7].setBounds(170, 135, 50, 50);
-        numberButtons[8].setBounds(5, 195, 50, 50);
-        numberButtons[9].setBounds(60, 195, 50, 50);
+        numberButtons[0].setBounds(xPos, 80, xWidth, yHeight);
+        numberButtons[1].setBounds(60, 80, xWidth, yHeight);
+        numberButtons[2].setBounds(115, 80, xWidth, yHeight);
+        numberButtons[3].setBounds(170, 80, xWidth, yHeight);
+        numberButtons[4].setBounds(xPos, 135, xWidth, yHeight);
+        numberButtons[5].setBounds(60, 135, xWidth, yHeight);
+        numberButtons[6].setBounds(115, 135, xWidth, yHeight);
+        numberButtons[7].setBounds(170, 135, xWidth, yHeight);
+        numberButtons[8].setBounds(xPos, 195, xWidth, yHeight);
+        numberButtons[9].setBounds(60, 195, xWidth, yHeight);
+        numberButtons[10].setBounds(115, 195, xWidth, yHeight);
+        numberButtons[11].setBounds(170, 195, xWidth, yHeight);
 
 
         //Add all number buttons to the frame
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 12; i++){
             this.add(numberButtons[i]);
         }
 
         //JButton negative "(-)"
-        JButton minusButton = new JButton("(-)");
-        minusButton.setBounds(170, 195, 50, 50);
-        this.add(minusButton);
+        JButton negButton = new JButton("+/-");
+        negButton.setBounds(170, 195, xWidth, xWidth);
+        this.add(negButton);
 
         // Function Buttons
          //JButton "."
          JButton pointButton = new JButton(".");
-         pointButton.setBounds(115, 195, 50, 50);
+         pointButton.setBounds(115, 195, xWidth, yHeight);
 
         //JButton '+' operation
         JButton addButton = new JButton("+");
-        addButton.setBounds(5, 255, 50, 25);
+        addButton.setBounds(5, 255, xWidth, yHeight);
 
         //JButton '-' operation
         JButton subButton = new JButton("-");
-        subButton.setBounds(60, 255, 50, 25);
+        subButton.setBounds(60, 255, xWidth, yHeight);
 
         //JButton 'x' operation
         JButton mulButton = new JButton("x");
-        mulButton.setBounds(115, 255, 50, 25);
+        mulButton.setBounds(115, 255, xWidth, yHeight);
 
         //JButton '%' operation
         JButton divButton = new JButton("/");
-        divButton.setBounds(170, 255, 50, 25);
+        divButton.setBounds(170, 255, xWidth, yHeight);
 
         //JButton to calculate the result
         JButton equalsToButton = new JButton("=");
-        equalsToButton.setBounds(85, 290, 50, 25);
+        equalsToButton.setBounds(xPos, 325, xWidth, yHeight);
         
         //JButton to clear all text fields
         JButton deleteButton = new JButton("Del");
-        deleteButton.setBounds(40, 335, 60, 20);
+        deleteButton.setBounds(60, 325, xWidth, yHeight);
 
         //JButton to clear all text fields
-        JButton clearButton = new JButton("Clear all");
-        clearButton.setBounds(120, 335, 80, 20);
+        JButton clearButton = new JButton("Clr");
+        clearButton.setBounds(115, 325, xWidth, yHeight);
 
         // An Array of Function Buttons
         JButton[] functionButtons = new JButton[8];
@@ -146,6 +168,7 @@ public class CalculatorWindow extends JFrame {
                     }
                     if (e.getSource() == subButton){
                         inputTextField.setText(inputTextField.getText() + "-");
+                        //subButton.setEnabled(false);
                     }
                     if (e.getSource() == mulButton){
                         inputTextField.setText(inputTextField.getText() + "*");
@@ -153,23 +176,26 @@ public class CalculatorWindow extends JFrame {
                     if (e.getSource() == divButton){
                         inputTextField.setText(inputTextField.getText() + "/");
                     }
+
                     if (e.getSource() == equalsToButton){
+                        double result = 0;
                         String input = inputTextField.getText();
                         String[] numbers = input.split("[+\\-*/]");
-                        double num1 = Double.parseDouble(numbers[0]);
+                        result = Double.parseDouble(numbers[0]);
+                        //double num1 = Double.parseDouble(numbers[0]);
                         double num2 = Double.parseDouble(numbers[1]);
-                        double result = 0;
+                        
                         if (input.contains("+")){
-                            result = num1 + num2;
+                            result = result + num2;
                         }
                         if (input.contains("-")){
-                            result = num1 - num2;
+                            result = result - num2;
                         }
                         if (input.contains("*")){
-                            result = num1 * num2;
+                            result = result * num2;
                         }
                         if (input.contains("/")){
-                            result = num1 / num2;
+                            result = result / num2;
                         }
                         outputTextField.setText(String.valueOf(result));
                     }
@@ -186,7 +212,7 @@ public class CalculatorWindow extends JFrame {
         }
 
         // Add action listener to all number buttons
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 11; i++){
             numberButtons[i].addActionListener(new ActionListener(){
                 @Override
                 public void actionPerformed(ActionEvent e){
@@ -196,11 +222,13 @@ public class CalculatorWindow extends JFrame {
             });
         }
 
-        // Add action listener to the minus button
-        minusButton.addActionListener(new ActionListener(){
+        // Add action listener to the negative button
+        negButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                inputTextField.setText(inputTextField.getText() + "-");
+                double temp = Double.parseDouble(inputTextField.getText());
+                inputTextField.setText(String.valueOf(temp * (-1)));
+                
             }
         });
 
@@ -212,7 +240,7 @@ public class CalculatorWindow extends JFrame {
                 outputTextField.setText("");
             }
         });
-
+        /* 
         // Add action listener to the delete button
         deleteButton.addActionListener(new ActionListener(){
             @Override
@@ -221,6 +249,7 @@ public class CalculatorWindow extends JFrame {
                 inputTextField.setText(text.substring(0, text.length() - 1));
             }
         });
+        */
 
 
         
